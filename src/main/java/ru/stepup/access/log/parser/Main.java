9097dc738818d7fa48d3da8651701d6673e91eef
Andrey Melnik <andreum.melnik@yandex.ru>
2024-07-17 20:07:30 +0300

    Задание по основам синтаксиса языка Java и базовыми типами данных

--- src/main/java/ru/stepup/access/log/parser/Main.java
+++ src/main/java/ru/stepup/access/log/parser/Main.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        System.out.println("Введите первое число:");
+        int firstValue = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondValue = new Scanner(System.in).nextInt();
+        double sum = firstValue + secondValue;
+
+        double difference = firstValue - secondValue;
+        double multiplication = firstValue * secondValue;
+        double split = firstValue / secondValue;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + split);
+
     }
 
 }
