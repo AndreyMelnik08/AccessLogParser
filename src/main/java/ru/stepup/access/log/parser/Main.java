7012630f2e8ac1d9ec7299b39b65ac5e8e520bad
Andrey Melnik <andreum.melnik@yandex.ru>
2024-07-25 14:06:16 +0300

    Задание по теме циклы

--- src/main/java/ru/stepup/access/log/parser/Main.java
+++ src/main/java/ru/stepup/access/log/parser/Main.java
@@ -1,7 +1,24 @@
 package ru.stepup.access.log.parser;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExist || isDirectory) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            count++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count);
+        }
     }
 }
