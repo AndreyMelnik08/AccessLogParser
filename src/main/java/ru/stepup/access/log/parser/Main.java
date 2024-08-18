5013666c339799a746ac3f2680f735e556298022
Andrey Melnik <andreum.melnik@yandex.ru>
2024-08-18 21:23:30 +0300

    Обработка исключений

--- src/main/java/ru/stepup/access/log/parser/Main.java
+++ src/main/java/ru/stepup/access/log/parser/Main.java
@@ -1,24 +1,42 @@
 package ru.stepup.access.log.parser;
 
-import java.io.File;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        int count = 0;
+    public static void main(String[] args) throws IOException {
 
-        while (true) {
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            boolean fileExist = file.exists();
-            boolean isDirectory = file.isDirectory();
-            if (!fileExist || isDirectory) {
-                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
-                continue;
+        String path = "C:\\Users\\Компутер\\IdeaProjects\\AccessLogParser\\access.log";
+
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            int totalLines = 0;
+            int maxLength = 0;
+            int minLength = 1024;
+            while ((line = reader.readLine()) != null) {
+                totalLines++;
+                int length = line.length();
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+                if (length < minLength) {
+                    minLength = length;
+                }
+                if (length > 1024) {
+                    throw new RuntimeException("В файле существует строка длиннее 1024 символов.");
+                }
             }
-            count++;
-            System.out.println("Путь указан верно");
-            System.out.println("Это файл номер " + count);
+            System.out.println("Общее количество строк в файле: " + totalLines);
+            System.out.println("Самая длинная строка: " + maxLength + " символов");
+            System.out.println("Самая короткая строка: " + minLength + " символов");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+
     }
 }
+
+
