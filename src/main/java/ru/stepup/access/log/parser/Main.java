acbb19529b9fc5ecf513bd18aa31189a51dd11d0
Andrey Melnik <andreum.melnik@yandex.ru>
2024-08-18 23:16:24 +0300

    Сравнение объектов

--- src/main/java/ru/stepup/access/log/parser/Main.java
+++ src/main/java/ru/stepup/access/log/parser/Main.java
@@ -6,37 +6,41 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         String path = "C:\\Users\\Компутер\\IdeaProjects\\AccessLogParser\\access.log";
-
         try {
             FileReader fileReader = new FileReader(path);
             BufferedReader reader = new BufferedReader(fileReader);
             String line;
             int totalLines = 0;
-            int maxLength = 0;
-            int minLength = 1024;
+            int googleBotCount = 0;
+            int yandexBotCount = 0;
             while ((line = reader.readLine()) != null) {
                 totalLines++;
                 int length = line.length();
-                if (length > maxLength) {
-                    maxLength = length;
-                }
-                if (length < minLength) {
-                    minLength = length;
-                }
                 if (length > 1024) {
                     throw new RuntimeException("В файле существует строка длиннее 1024 символов.");
                 }
+                String[] parts = line.split("\\(");
+                if (parts.length >= 2) {
+                    String fragment = parts[1];
+                    String[] fragments = fragment.split(";");
+                    if (fragments.length >= 2) {
+                        String userAgent = fragments[1].trim();
+                        String botName = userAgent.split("/")[0];
+                        if (botName.equals("Googlebot")) {
+                            googleBotCount++;
+                        } else if (botName.equals("YandexBot")) {
+                            yandexBotCount++;
+                        }
+                    }
+                }
             }
             System.out.println("Общее количество строк в файле: " + totalLines);
-            System.out.println("Самая длинная строка: " + maxLength + " символов");
-            System.out.println("Самая короткая строка: " + minLength + " символов");
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            System.out.println("Количество запросов от Googlebot: " + googleBotCount);
+            System.out.println("Количество запросов от YandexBot: " + yandexBotCount);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
-
 
